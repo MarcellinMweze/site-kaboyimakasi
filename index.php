3902319946cef18b7a39c0f0ba902aaa234a69a9
Marcellin_M <irmarcellinmweze@gmail.com>
2023-11-24 10:13:59 +0100

    Mon premier commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <div class="entete">
+            <div class="section entete">
+                <div class="entete-gauche">
+                    <p>Votre Solution pour une Construction Moderne</p>
+                    <span class="icone gauche"><i class="fa-solid fa-phone"></i> +243990548021</span>
+                </div>
+                <div class="entete-droite">
+                    <span class="icone"><a href=""><i class="fa-solid fa-moon"></i></a></span>
+                    <span class="icone"><a href=""><i class="fa-brands fa-facebook"></i></a></span>
+                    <span class="icone"><a href=""><i class="fa-brands fa-whatsapp"></i></a></span>
+                </div>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="section nav">
+                <div class="logo">
+                    <a href=""><img src="./images/Logo.jpeg" alt="Logo de la Société"></a>
+                    <span><strong>KABOYI MAKASI ENGINEERING SARL</strong></span>
+                </div>
+                <div class="liens">
+                    <ul>
+                        <li><a href="">Acceuil</a></li>
+                        <li><a href="">A props</a></li>
+                        <li><a href="">Nos Projets</a></li>
+                        <li><a href="">Contactez-nous</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="presentation">
+            <div class="section">
+                <div>
+                    <h3>Entreprise de Contruction professionnelle</h3>
+                    <h1>Nous construisons <br><span class="reve">vos reves</span></h1>
+                    <button class="btn contact">Nous contacter</button>
+                </div>
+                <div class="imgbatiment">
+                    <img src="./images/batiment1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="apropos">
+            <div class="section">
+                <div class="contener-apropos">
+                    <h2>Aperçu</h2>
+                    <div class="contener-apercu">
+                        <div class="apercu">
+                            <p>L'entreprise KABOYI MAKASI ENGINEERING SARL est une entreprise de construction dédiée à tous types de travaux publics tel que les travaux routiers, le terrassement, les études topographiques et géotchniques, le génie civil, les travaux de nettoyages (cleaning), h'hydraulique, les bâtiments, l'électricité, les ponts et chaussées, la protection de l'environnement, l'approvisionnement en eau potable et assainissement, l'hurbanisme et aménagement du territoire, etc</p>
+                            <p>Elle est également dédiée à tous les services liés directement ou indirectement à la construction et à la fourniture d'équipement et des matériaux de construction</p>
+                            <p>Nous reconnaissons que nos clients exigent de bons services, un produit de qualité et à un prix compétitif dans un délai limité.</p>
+                        </div>
+                        <div class="imgapropos">
+                            <img src="./images/station2.jpg" alt="Photo d'une Station">
+                        </div>
+                    </div>
+                    <h2>Notre Vision</h2>
+                    <div class="contener-vision">
+                        <div class="imgapropos">
+                            <img src="./images/batiment5.jpg" alt="Photo d'une Station">
+                        </div>
+                        <div class="vision">
+                            <p>La vision de l'Entreprise KABOYI MAKASI ENGINEERING SARL est de devenir l'entreprise la plus réputée d'Afrique en générale et de la République Démocratique du Congo en particulier ; motivée par l'innovation, pour fournir le développement qui mène à la croissance durable pour les populations et l'économie de l'Afrique et de la RD Congo</p>
+                        </div>
+                    </div>
+                    <h2>Nos Valeurs</h2>
+                    <div class="contener-valeur">
+                        <div class="valeur">
+                            <p>Les valeurs fondamentales de l'Entreprise KABOYI MAKASI ENGINEERING SARL définissent la manière dont nous devons agir en tant qu'un individu et en tant qu'une équipe de travail pour atteindre nos objectifs commerciaux.</p>
+                            <p>Faire cequi est juste : nous nous engageons à respecter les normes de conduite d'éthique les plus élevées dans tout ce que nous entreprenons ; nous crouyons que l'honnêteté et l'intégrité engendre la confiance, qui est la pière angulaire de notre entreprise. Nous nous efforçons d'être de bons citoyens et nous assumons la responsabilité de nos actions.</p>
+                            <p>Nous reconnaissons que notre succès en tant qu'entrprise dépend du talent, des compétences et de l'expertise de nos employés et de notre capacité à fonctionner comme une équipe étroitement intégrée</p>
+                            <p>Nous apprécions notre diversité et crouyons que le respect, pour nos collègues, nos clients, nos partenaires et tous ceux avec qui nous interagissons, est un élément essentiel de toute relation commerciale positive et productive.</p>
+                            <p>Nous comprenons l'importance de nos projets et la confiance que nos clients nous accordent. Dans l'esprit, nous nous efforçons d'exceller dans tous les aspects de notre expertise et abordons chaque défi avec la détermination de réussir.</p>
+                            <p>Nous prenons en charge la conduite de haute performance à travers la livraison de nos projets</p>
+                        </div>
+                        <div class="imgapropos">
+                            <img src="./images/batiment4.jpg" alt="Photo d'une Station">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="footer">
+        
+    </div>
+    </main>
+</body>
+
+</html>
