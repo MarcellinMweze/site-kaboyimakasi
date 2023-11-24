0d8198e42f10cda62644d49f7e4fae2020721255
Marcellin_M <irmarcellinmweze@gmail.com>
2023-11-24 13:54:23 +0100

    Ajout éléments à propos

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                 <div class="liens">
                     <ul>
                         <li><a href="">Acceuil</a></li>
-                        <li><a href="">A props</a></li>
+                        <li><a href="">A propos</a></li>
                         <li><a href="">Nos Projets</a></li>
                         <li><a href="">Contactez-nous</a></li>
                     </ul>
@@ -51,10 +51,10 @@
                 </div>
             </div>
         </div>
-        <div class="apropos">
+        <div class="apercu">
             <div class="section">
-                <div class="contener-apropos">
-                    <h2>Aperçu</h2>
+                <h2>Aperçu</h2>
+                <div class="contener-apercu">
                     <div class="contener-apercu">
                         <div class="apercu">
                             <p>L'entreprise KABOYI MAKASI ENGINEERING SARL est une entreprise de construction dédiée à tous types de travaux publics tel que les travaux routiers, le terrassement, les études topographiques et géotchniques, le génie civil, les travaux de nettoyages (cleaning), h'hydraulique, les bâtiments, l'électricité, les ponts et chaussées, la protection de l'environnement, l'approvisionnement en eau potable et assainissement, l'hurbanisme et aménagement du territoire, etc</p>
@@ -65,6 +65,12 @@
                             <img src="./images/station2.jpg" alt="Photo d'une Station">
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="vision">
+            <div class="contener-vision">
+                <div class="section">
                     <h2>Notre Vision</h2>
                     <div class="contener-vision">
                         <div class="imgapropos">
@@ -74,26 +80,66 @@
                             <p>La vision de l'Entreprise KABOYI MAKASI ENGINEERING SARL est de devenir l'entreprise la plus réputée d'Afrique en générale et de la République Démocratique du Congo en particulier ; motivée par l'innovation, pour fournir le développement qui mène à la croissance durable pour les populations et l'économie de l'Afrique et de la RD Congo</p>
                         </div>
                     </div>
-                    <h2>Nos Valeurs</h2>
-                    <div class="contener-valeur">
-                        <div class="valeur">
-                            <p>Les valeurs fondamentales de l'Entreprise KABOYI MAKASI ENGINEERING SARL définissent la manière dont nous devons agir en tant qu'un individu et en tant qu'une équipe de travail pour atteindre nos objectifs commerciaux.</p>
-                            <p>Faire cequi est juste : nous nous engageons à respecter les normes de conduite d'éthique les plus élevées dans tout ce que nous entreprenons ; nous crouyons que l'honnêteté et l'intégrité engendre la confiance, qui est la pière angulaire de notre entreprise. Nous nous efforçons d'être de bons citoyens et nous assumons la responsabilité de nos actions.</p>
-                            <p>Nous reconnaissons que notre succès en tant qu'entrprise dépend du talent, des compétences et de l'expertise de nos employés et de notre capacité à fonctionner comme une équipe étroitement intégrée</p>
-                            <p>Nous apprécions notre diversité et crouyons que le respect, pour nos collègues, nos clients, nos partenaires et tous ceux avec qui nous interagissons, est un élément essentiel de toute relation commerciale positive et productive.</p>
-                            <p>Nous comprenons l'importance de nos projets et la confiance que nos clients nous accordent. Dans l'esprit, nous nous efforçons d'exceller dans tous les aspects de notre expertise et abordons chaque défi avec la détermination de réussir.</p>
-                            <p>Nous prenons en charge la conduite de haute performance à travers la livraison de nos projets</p>
-                        </div>
-                        <div class="imgapropos">
-                            <img src="./images/batiment4.jpg" alt="Photo d'une Station">
-                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="nosvaleurs">
+            <div class="section">
+                <h2>Nos Valeurs</h2>
+                <div class="contener-valeur">
+                    <div class="valeur">
+                        <p>Les valeurs fondamentales de l'Entreprise KABOYI MAKASI ENGINEERING SARL définissent la manière dont nous devons agir en tant qu'un individu et en tant qu'une équipe de travail pour atteindre nos objectifs commerciaux.</p>
+                        <p>Faire cequi est juste : nous nous engageons à respecter les normes de conduite d'éthique les plus élevées dans tout ce que nous entreprenons ; nous crouyons que l'honnêteté et l'intégrité engendre la confiance, qui est la pière angulaire de notre entreprise. Nous nous efforçons d'être de bons citoyens et nous assumons la responsabilité de nos actions.</p>
+                        <p>Nous reconnaissons que notre succès en tant qu'entrprise dépend du talent, des compétences et de l'expertise de nos employés et de notre capacité à fonctionner comme une équipe étroitement intégrée</p>
+                        <p>Nous apprécions notre diversité et crouyons que le respect, pour nos collègues, nos clients, nos partenaires et tous ceux avec qui nous interagissons, est un élément essentiel de toute relation commerciale positive et productive.</p>
+                        <p>Nous comprenons l'importance de nos projets et la confiance que nos clients nous accordent. Dans l'esprit, nous nous efforçons d'exceller dans tous les aspects de notre expertise et abordons chaque défi avec la détermination de réussir.</p>
+                        <p>Nous prenons en charge la conduite de haute performance à travers la livraison de nos projets</p>
+                    </div>
+                    <div class="imgapropos">
+                        <img src="./images/batiment4.jpg" alt="Photo d'une Station">
                     </div>
                 </div>
             </div>
         </div>
+        <div class="domaine">
+            <div class="section">
+                <h2>Ce que nous faisons</h2>
+                <div class="faire">
+                    <p><i class="fa-regular fa-circle-check"></i>Génie Civil et Construction</p>
+                    <p><i class="fa-regular fa-circle-check"></i>Projets d'installations industrielles et électriques</p>
+                    <p><i class="fa-regular fa-circle-check"></i>Travaux d'alimentation, de distribution et traitement des eaux</p>
+                    <p><i class="fa-regular fa-circle-check"></i>Les étutes topographiques et géotechniques</p>
+                    <p><i class="fa-regular fa-circle-check"></i>Les projets d'innovations</p>
+                    <p><i class="fa-regular fa-circle-check"></i>L'expertise d'évaluation immobilière</p>
+                    <p><i class="fa-regular fa-circle-check"></i>La conception et production des plans architecturaux</p>
+                    <p><i class="fa-regular fa-circle-check"></i>Les commerces généraux</p>
+                </div>
+            </div>
+        </div>
         <div class="footer">
-            <div class="footer-contener">
-                <p>Marcellin</p>
+            <div class="section">
+                <div class="contener-footer">
+                    <div class="expertise">
+                        <h3>Notre Expertise</h3>
+                        <p>Notre expertise est une compréhension approfondie des travaux de Génie Civil et des besoins géographiquement spécifiques; notre expérience et notre myriade de solution clients nous laissent dans une position unique pour devenir la première société de Génie Civil et de Construction de la région.</p>
+                    </div>
+                    <div class="aide">
+                        <h3>Aide</h3>
+                        <ul>
+                            <li><a href="">Acceuil</a></li>
+                            <li><a href="">A Propos</a></li>
+                            <li><a href="">Nos Projets</a></li>
+                            <li><a href="">Contactez-nous</a></i>
+                        </ul>
+                    </div>
+                    <div class="contact">
+                        <h3>Contact</h3>
+                        <p>Entre 16 et 17ième Poteau, Route Kinsevere, Lubumbashi-RD Congo</p>
+                        <p>km.engineeringsarl@kaboyimakasi.com</p>
+                        <p>+243990548021</p>
+                        <p>www.kaboyimakasi.com</p>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
