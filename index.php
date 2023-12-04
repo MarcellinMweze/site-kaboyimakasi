2e12ab402ff0f662449d0217bfc729b3d3d8c20a
Marcellin_M <irmarcellinmweze@gmail.com>
2023-12-04 13:01:48 +0100

    Modification navbar et index

--- index.php
+++ index.php
@@ -19,14 +19,14 @@ include('./header.php');
                 </div>
             </div>
         </div>
-        <div class="apercu"> 
+        <div class="apercu">
             <div class="section">
                 <h2 id="apropos">Aperçu</h2>
                 <div class="contener-apercu">
                     <div class="contener-apercu">
                         <div class="apercu">
-                            <p>L'entreprise KABOYI MAKASI ENGINEERING SARL est une entreprise de construction dédiée à tous types de travaux publics tel que les travaux routiers, le terrassement, les études topographiques et géotchniques, le génie civil, les travaux de nettoyages (cleaning), h'hydraulique, les bâtiments, l'électricité, les ponts et chaussées, la protection de l'environnement, l'approvisionnement en eau potable et assainissement, l'hurbanisme et aménagement du territoire, etc</p>
-                            <p>Elle est également dédiée à tous les services liés directement ou indirectement à la construction et à la fourniture d'équipement et des matériaux de construction</p>
+                            <p>L'entreprise KABOYI MAKASI ENGINEERING SARL est une entreprise de construction dédiée à tous types de travaux publics tel que les travaux routiers, le terrassement, les études topographiques et géotechniques, le génie civil, les travaux de nettoyages (cleaning), l'hydraulique, les bâtiments, l'électricité, les ponts et chaussées, la protection de l'environnement, l'approvisionnement en eau potable et assainissement, l'hurbanisme et aménagement du territoire, etc</p>
+                            <p>Elle est également dédiée à tous les services liés directement ou indirectement à la construction et à la fourniture d'équipement et des matériaux de construction.</p>
                             <p>Nous reconnaissons que nos clients exigent de bons services, un produit de qualité et à un prix compétitif dans un délai limité.</p>
                         </div>
                         <div class="imgapropos">
@@ -45,7 +45,7 @@ include('./header.php');
                             <img src="./images/batiment5.jpg" alt="Photo d'une Station">
                         </div>
                         <div class="vision">
-                            <p>La vision de l'Entreprise KABOYI MAKASI ENGINEERING SARL est de devenir l'entreprise la plus réputée d'Afrique en générale et de la République Démocratique du Congo en particulier ; motivée par l'innovation, pour fournir le développement qui mène à la croissance durable pour les populations et l'économie de l'Afrique et de la RD Congo</p>
+                            <p>La vision de l'Entreprise KABOYI MAKASI ENGINEERING SARL est de devenir l'entreprise la plus réputée d'Afrique en générale et de la République Démocratique du Congo en particulier ; motivée par l'innovation, pour fournir le développement qui mène à la croissance durable pour les populations et l'économie de l'Afrique et de la RD Congo en particulier.</p>
                         </div>
                     </div>
                 </div>
@@ -57,11 +57,11 @@ include('./header.php');
                 <div class="contener-valeur">
                     <div class="valeur">
                         <p>Les valeurs fondamentales de l'Entreprise KABOYI MAKASI ENGINEERING SARL définissent la manière dont nous devons agir en tant qu'un individu et en tant qu'une équipe de travail pour atteindre nos objectifs commerciaux.</p>
-                        <p>Faire cequi est juste : nous nous engageons à respecter les normes de conduite d'éthique les plus élevées dans tout ce que nous entreprenons ; nous crouyons que l'honnêteté et l'intégrité engendre la confiance, qui est la pière angulaire de notre entreprise. Nous nous efforçons d'être de bons citoyens et nous assumons la responsabilité de nos actions.</p>
-                        <p>Nous reconnaissons que notre succès en tant qu'entrprise dépend du talent, des compétences et de l'expertise de nos employés et de notre capacité à fonctionner comme une équipe étroitement intégrée</p>
-                        <p>Nous apprécions notre diversité et crouyons que le respect, pour nos collègues, nos clients, nos partenaires et tous ceux avec qui nous interagissons, est un élément essentiel de toute relation commerciale positive et productive.</p>
+                        <p>Faire cequi est juste : nous nous engageons à respecter les normes de conduite d'éthique les plus élevées dans tout ce que nous entreprenons ; nous croyons que l'honnêteté et l'intégrité engendre la confiance, qui est la pierre angulaire de notre entreprise. Nous nous efforçons d'être de bons citoyens et nous assumons la responsabilité de nos actions.</p>
+                        <p>Nous reconnaissons que notre succès en tant qu'entrprise dépend du talent, des compétences et de l'expertise de nos employés et de notre capacité à fonctionner comme une équipe étroitement intégrée.</p>
+                        <p>Nous apprécions notre diversité et croyons que le respect, pour nos collègues, nos clients, nos partenaires et tous ceux avec qui nous interagissons, est un élément essentiel de toute relation commerciale positive et productive.</p>
                         <p>Nous comprenons l'importance de nos projets et la confiance que nos clients nous accordent. Dans l'esprit, nous nous efforçons d'exceller dans tous les aspects de notre expertise et abordons chaque défi avec la détermination de réussir.</p>
-                        <p>Nous prenons en charge la conduite de haute performance à travers la livraison de nos projets</p>
+                        <p>Nous prenons en charge la conduite de haute performance à travers la livraison de nos projets.</p>
                     </div>
                     <div class="imgapropos">
                         <img src="./images/batiment4.jpg" alt="Photo d'une Station">
